ef3ef8d2a08efe29443365fc26af0d6cf581268e
vankaspr <msvankaspr@gmail.com>
2025-08-08 20:00:20 +0500

    ✅ Create main js file

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+
+try {
+    require('electron-reloader')(module, {
+        watchRenderer: true
+    })
+} catch {}
+
+async function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
+
+    try {
+        // Обрабатываем Promise от loadFile
+        await win.loadFile(path.join(__dirname, 'index.html'))
+        console.log('Окно успешно загружено')
+    } catch (err) {
+        console.error('Ошибка загрузки файла:', err)
+    }
+}
+
+app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required')
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
