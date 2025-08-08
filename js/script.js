6c77b5b7289db8d979074b96ce668d9726fd6537
vankaspr <msvankaspr@gmail.com>
2025-08-08 22:38:40 +0500

    📌 add music control

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const button = document.querySelector('.start-button');
 const clickSound = document.getElementById('clickSound');
 
+
+
+
 button.addEventListener('click', () => {
     clickSound.currentTime = 0;
     clickSound.play();
@@ -9,7 +12,37 @@ button.addEventListener('click', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     const bgMusic = document.getElementById('bgMusic');
+    const playBtn = document.getElementById('playBtn');
+    const pauseBtn = document.getElementById('pauseBtn');
+
     bgMusic.volume = 0.3;
+    let isPlaying = false;
+
+    pauseBtn.addEventListener('click', () => {
+        bgMusic.pause();
+        isPlaying = false;
+        toggleButtons();
+    })
+
+    playBtn.addEventListener('click', () => {
+        bgMusic.play();
+        isPlaying = true;
+        toggleButtons();
+    })
+
+    function toggleButtons() {
+        if (isPlaying) {
+            playBtn.style.display = 'none';
+            pauseBtn.style.display = 'block';
+            pauseBtn.classList.add('fade-anim');
+            setTimeout(() => pauseBtn.classList.remove('fade-anim'), 300);
+        } else {
+            pauseBtn.style.display = 'none';
+            playBtn.style.display = 'block';
+            playBtn.classList.add('fade-anim');
+            setTimeout(() => playBtn.classList.remove('fade-anim'), 300);
+        }
+    }
 
     const playMusic = () => {
         bgMusic.play().catch(e => console.log("Auto play NOT PERMISSION"));
@@ -17,8 +50,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     playMusic();
 
-    //document.body.addEventListener('click', () => {
-    //    playMusic();
-    //    document.body.removeEventListener('click', playMusic);
-    //}, { once: true});
 })
