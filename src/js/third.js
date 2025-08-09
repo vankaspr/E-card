b925c26d698fd21733bb72f747dd2e330c1db24d
vankaspr <msvankaspr@gmail.com>
2025-08-09 17:15:08 +0500

    📌 create third page

---
+++ src/js/third.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const thirdPageBtn= document.querySelector(".third-button")
+    const clickSound = document.getElementById('clickSound');
+
+    thirdPageBtn.addEventListener('click', async () => {
+
+        clickSound.currentTime = 0;
+        clickSound.play().catch(e => console.error("Ошибка звука:", e));
+
+        // Переход после начала воспроизведения
+        setTimeout(() => window.electronAPI.navigateTo('fourth'), 300);
+    });
+});
